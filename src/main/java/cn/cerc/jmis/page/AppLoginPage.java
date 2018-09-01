fde4cb05254dca6ef4778bb82f9a0a4c386a8738
黄荣君 <l1091462907@gmail.com>
2018-09-02 02:06:02 +0800

    fix docker port

--- src/main/java/cn/cerc/jmis/page/AppLoginPage.java
+++ src/main/java/cn/cerc/jmis/page/AppLoginPage.java
@@ -82,7 +82,7 @@ public class AppLoginPage extends AbstractJspPage implements IAppLogin {
 
         String ip = config.getProperty(notify_ip);
         if (ip != null || !"".equals(ip)) {
-            int localPort = form.getRequest().getLocalPort();
+            int localPort = form.getRequest().getServerPort();
             String notify_url = "http://";
             if (localPort != 80) {
                 notify_url += (ip + ":" + localPort);
