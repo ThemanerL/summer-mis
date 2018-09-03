f576b883614d47738e5bfd12f61db62a94096134
黄荣君 <l1091462907@gmail.com>
2018-09-03 14:44:24 +0800

    Revert "删除不必要地址过滤"
    This reverts commit 448ae1bb8dd177555f3b56fb9a2089b3c1d6ed49.

--- src/main/java/cn/cerc/jmis/page/qrcode/SocketTool.java
+++ src/main/java/cn/cerc/jmis/page/qrcode/SocketTool.java
@@ -1,8 +1,18 @@
 package cn.cerc.jmis.page.qrcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 public class SocketTool {
+
+    // FIXME 改为参数传入
+    private static List<String> items = new ArrayList<>();
+    static {
+        items.add("diteng");
+        items.add("knowall");
+    }
 
     public String getDomain(HttpServletRequest request) {
         StringBuffer url = request.getRequestURL();
@@ -21,6 +31,15 @@ public class SocketTool {
         String socketUrl = domain;
         if (domain.indexOf("http://") != -1) {
             socketUrl = domain.replace("http", "ws");
+
+            // 遇到指定域名 websocket 开启 ssl
+            for (String item : items) {
+                if (socketUrl.indexOf(item) != -1) {
+                    socketUrl = socketUrl.replace("ws", "wss");
+                    continue;
+                }
+            }
+
         } else if (domain.indexOf("https://") != -1) {
             socketUrl = domain.replace("https", "wss");
         }
