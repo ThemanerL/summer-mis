ad874dc5746dd1919951982589722e2aeb3449a1
ZhangGong <sz9214e@qq.com>
2018-12-08 11:33:47 +0800

    改进代码

--- src/main/java/cn/cerc/mis/core/IBookOption.java
+++ src/main/java/cn/cerc/mis/core/IBookOption.java
@@ -1,13 +1,7 @@
 package cn.cerc.mis.core;
 
-import cn.cerc.core.IHandle;
-
 public interface IBookOption extends IOption {
     default String getBook() {
         return null;
-    }
-
-    default boolean getValue(IHandle handle) {
-        return true;
     }
 }
