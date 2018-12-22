9da7a30785909085a8512d5192aa0f856161cde1
ZhangGong <sz9214e@qq.com>
2018-12-22 19:42:47 +0800

    恢复旧版本

--- src/main/java/cn/cerc/mis/core/StartApp.java
+++ src/main/java/cn/cerc/mis/core/StartApp.java
@@ -2,72 +2,77 @@ package cn.cerc.mis.core;
 
 import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.WebApplicationContext;
 
 import cn.cerc.core.IHandle;
 import cn.cerc.db.core.IAppConfig;
 
-@Controller
-@Scope(WebApplicationContext.SCOPE_REQUEST)
-@RequestMapping("/")
-public class StartApp {
-    @Autowired
-    private HttpServletRequest req;
-    @Autowired
-    private HttpServletResponse resp;
+@Deprecated // 请改使用 StartAppDefault
+public class StartApp implements Filter {
+    // private static final Logger log = Logger.getLogger(AppStart.class);
 
-    @RequestMapping("/")
-    public String doGet() {
-        if (req.getParameter(ClientDevice.deviceId_key) != null)
-            req.getSession().setAttribute(ClientDevice.deviceId_key, req.getParameter(ClientDevice.deviceId_key));
-        if (req.getParameter(ClientDevice.deviceType_key) != null)
-            req.getSession().setAttribute(ClientDevice.deviceType_key, req.getParameter(ClientDevice.deviceType_key));
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
 
-        IAppConfig conf = Application.getAppConfig();
-        String jspFile = String.format("redirect：/%s/%s", conf.getPathForms(), conf.getFormWelcome());
-        return jspFile;
-    }
+        String uri = req.getRequestURI();
 
-    @RequestMapping("/MobileConfig")
-    @Deprecated
-    public String MobileConfig() {
-        return mobileConfig();
-    }
+        // 处理默认首页问题
+        if (uri.equals("/")) {
+            if (req.getParameter(ClientDevice.deviceId_key) != null)
+                req.getSession().setAttribute(ClientDevice.deviceId_key, req.getParameter(ClientDevice.deviceId_key));
+            if (req.getParameter(ClientDevice.deviceType_key) != null)
+                req.getSession().setAttribute(ClientDevice.deviceType_key,
+                        req.getParameter(ClientDevice.deviceType_key));
 
-    @RequestMapping("/mobileConfig")
-    public String mobileConfig() {
-        if (req.getParameter(ClientDevice.deviceId_key) != null)
-            req.getSession().setAttribute(ClientDevice.deviceId_key, req.getParameter(ClientDevice.deviceId_key));
-        if (req.getParameter(ClientDevice.deviceType_key) != null)
-            req.getSession().setAttribute(ClientDevice.deviceType_key, req.getParameter(ClientDevice.deviceType_key));
-        try {
-            IForm form;
-            if (Application.getContext().containsBean("mobileConfig"))
-                form = Application.getBean("mobileConfig", IForm.class);
-            else
-                form = Application.getBean("MobileConfig", IForm.class);
-            form.setRequest(req);
-            form.setResponse(resp);
-
-            IHandle handle = Application.getHandle();
-            handle.setProperty(Application.sessionId, req.getSession().getId());
-            form.setHandle(handle);
-            IPage page = form.execute();
-            return page.execute();
-        } catch (Exception e) {
+            IAppConfig conf = Application.getAppConfig();
+            resp.sendRedirect(String.format("/%s/%s", conf.getPathForms(), conf.getFormWelcome()));
+            return;
+        } else if (uri.equals("/MobileConfig") || uri.equals("/mobileConfig")) {
+            if (req.getParameter(ClientDevice.deviceId_key) != null)
+                req.getSession().setAttribute(ClientDevice.deviceId_key, req.getParameter(ClientDevice.deviceId_key));
+            if (req.getParameter(ClientDevice.deviceType_key) != null)
+                req.getSession().setAttribute(ClientDevice.deviceType_key,
+                        req.getParameter(ClientDevice.deviceType_key));
             try {
+                IForm form;
+                if (Application.getContext().containsBean("mobileConfig"))
+                    form = Application.getBean("mobileConfig", IForm.class);
+                else
+                    form = Application.getBean("MobileConfig", IForm.class);
+                form.setRequest((HttpServletRequest) request);
+                form.setResponse((HttpServletResponse) response);
+
+                IHandle handle = Application.getHandle();
+                handle.setProperty(Application.sessionId, req.getSession().getId());
+                form.setHandle(handle);
+                IPage page = form.execute();
+                page.execute();
+            } catch (Exception e) {
                 resp.getWriter().print(e.getMessage());
-            } catch (IOException e1) {
-                e1.printStackTrace();
             }
-            return null;
+            return;
         }
+
+        chain.doFilter(req, resp);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }
