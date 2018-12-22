bb6a326778b13fe7abe964491fce1f6d89ba23d6
ZhangGong <sz9214e@qq.com>
2018-12-22 19:56:32 +0800

    fix error

--- src/main/java/cn/cerc/mis/core/StartAppDefault.java
+++ src/main/java/cn/cerc/mis/core/StartAppDefault.java
@@ -6,17 +6,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.WebApplicationContext;
 
 import cn.cerc.core.IHandle;
 import cn.cerc.db.core.IAppConfig;
 
-@Controller
-@Scope(WebApplicationContext.SCOPE_REQUEST)
-@RequestMapping("/")
+//@Controller
+//@Scope(WebApplicationContext.SCOPE_REQUEST)
+//@RequestMapping("/")
 public class StartAppDefault {
     @Autowired
     private HttpServletRequest req;
