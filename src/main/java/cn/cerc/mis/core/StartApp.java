730f26e08a9ff585f79772b0fbcfe8cff6e3d9b6
黄荣君 <l1091462907@gmail.com>
2019-11-28 21:18:23 +0800

    Update StartApp.java

--- src/main/java/cn/cerc/mis/core/StartApp.java
+++ src/main/java/cn/cerc/mis/core/StartApp.java
@@ -11,12 +11,16 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import cn.cerc.core.IHandle;
 import cn.cerc.db.core.IAppConfig;
 
 @Deprecated // 请改使用 StartAppDefault
 public class StartApp implements Filter {
-    // private static final Logger log = Logger.getLogger(AppStart.class);
+
+    private static final Logger log = LoggerFactory.getLogger(StartApp.class);
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -62,6 +66,9 @@ public class StartApp implements Filter {
                 resp.getWriter().print(e.getMessage());
             }
             return;
+        } else {
+            StringBuffer url = req.getRequestURL();
+            log.info("{}", url.toString());
         }
 
         chain.doFilter(req, resp);
